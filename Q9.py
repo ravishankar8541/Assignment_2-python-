f63daf49f571fcb288bf612195cfbbb69a6dd6a3
ravishankar8541 <107765864+ravishankar8541@users.noreply.github.com>
2022-09-11 19:21:37 +0530

    Add files via upload

---
+++ Q9.py
@@ -0,0 +1,10 @@
+#Write a python script to create variables to store your name, age, qualification, years
+#of experience and print it.
+name="Ravi Shankar Kumar Ray";
+age=21;
+quali="12th";
+year=2;
+print("Your name is ",name);
+print("Your age is ",age);
+print("Your Qualification is",quali);
+print("Your Experience is",year," years")
