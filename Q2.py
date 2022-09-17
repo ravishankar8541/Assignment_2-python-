fdb0950640a0bf054b9d0eaf49a40c06c691d550
ravishankar8541 <107765864+ravishankar8541@users.noreply.github.com>
2022-09-17 19:04:55 +0530

    Add files via upload

--- Q2.py
+++ Q2.py
@@ -1,1 +1,14 @@
-x=5
+"""Write a python script to add multi line comments and print values of four variables,
+each in a new line. Variable contains any values."""
+"""
+a=6
+b=9
+c=4
+ """
+
+a=6
+b=9
+c=4
+print(a)
+print(b)
+print(c)
