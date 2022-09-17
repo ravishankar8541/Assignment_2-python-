ca2fdb6edf16974bee1d8f722f6b19ef1f5505c1
ravishankar8541 <107765864+ravishankar8541@users.noreply.github.com>
2022-09-17 19:03:38 +0530

    Add files via upload

---
+++ Q5.py
@@ -0,0 +1,11 @@
+"""Create four variables in a Python script and assign values of different data types to
+them. Write a Python script to print value, its type and id of each variable"""
+
+a=7
+b="7"
+print(a)
+print(b)
+print(type(a))
+print(type(b))
+print(id(a))
+print(id(b))
